1f8827c2a2da7f253bab1c48959812817eccfc8c
malassila <malassila@wccnet.edu>
2024-01-29 11:17:10 -0500

    Initial commit

---
+++ src/main/java/io/pcsp/SpringDriveAPI/SpringDriveApiApplication.java
@@ -0,0 +1,13 @@
+package io.pcsp.SpringDriveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDriveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDriveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pcsp/SpringDriveAPI/SpringDriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pcsp.SpringDriveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDriveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
